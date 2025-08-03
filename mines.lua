4c510f0cff88bc830b401847471bca9261399426
TokkenDev <153781013+TokkenDev@users.noreply.github.com>
2025-08-04 00:12:46 +0200

    Create mines.lua

---
+++ mines.lua
@@ -0,0 +1,144 @@
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+local Window = OrionLib:MakeWindow({Name = "clock.lua", HidePremium = false, SaveConfig = true, ConfigFolder = "clock.lua.mines"})
+local MineTab = Window:MakeTab({
+    Name = "Mining",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+-- Init
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local Mine = ReplicatedStorage["shared/network/MiningNetwork@GlobalMiningEvents"].Mine
+local items = workspace:FindFirstChild("Items")
+
+-- Variables
+local AutoMine = false
+local ColOres = false
+local MiningStrength = 1 -- Default to Max
+local MiningThread = nil
+local OresThread = nil
+
+-- Functions
+local function MineOres()
+    while AutoMine do
+        local camera = workspace.CurrentCamera.CFrame.LookVector
+        local minePos = Vector3.new(
+            math.round(math.clamp(camera.X * 10, -10, 10)),
+            math.round(math.clamp(camera.Y * 10, -10, 10)),
+            math.round(math.clamp(camera.Z * 10, -10, 10))
+        )
+        Mine:FireServer(minePos, MiningStrength)
+        task.wait(0.1)
+        print("Mined")
+    end
+end
+
+local function CollectOres()
+    local miningNetwork = ReplicatedStorage:FindFirstChild("shared/network/MiningNetwork@GlobalMiningEvents")
+    local collectItem = miningNetwork and miningNetwork:FindFirstChild("CollectItem")
+    while ColOres do
+        local items = items:GetChildren()
+        if #items > 0 then
+            for _, item in ipairs(items) do
+                if not settings.auto_collect_ores then
+                    break
+                end
+                local success, err = pcall(function()
+                    collectItem:FireServer(item.Name)
+                end)
+                if not success then
+                    warn("Error collecting item:", err)
+                else
+                    print("Collected item:", item.Name)
+                end
+                task.wait()
+            end
+        end
+    end
+end
+
+-- Tab Elements
+local AutoMineToggle = MineTab:AddToggle({Name = "Auto Mine",  Default = false,  Callback = function(bool)
+    AutoMine = bool
+    if AutoMine then
+        if MiningStrength then
+            MiningThread = task.spawn(MineOres)
+            OrionLib:MakeNotification({
+                Name = "Auto Mining",
+                Content = "Auto mining is now active.",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        else
+            OrionLib:MakeNotification({
+                Name = "Mining Strength Missing!",
+                Content = "Please set the mining strength first.",
+                Image = "rbxassetid://4483345998",
+                Time = 5
+            })
+            AutoMine = false
+            AutoMineToggle:Set(false)
+        end
+    else
+        if MiningThread then
+            task.cancel(MiningThread)
+            MiningThread = nil
+            OrionLib:MakeNotification({
+                Name = "Auto Mining",
+                Content = "Auto mining has been disabled.",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        end
+    end
+end})
+
+MineTab:AddDropdown({
+    Name = "Mining Strength", 
+    Default = "Max", 
+    Options = {"Max", "Good", "Decent", "Bad"}, 
+    Callback = function(val)
+        if val == "Max" then
+            MiningStrength = 1
+        elseif val == "Good" then
+            MiningStrength = 0.8
+        elseif val == "Decent" then
+            MiningStrength = 0.7
+        elseif val == "Bad" then
+            MiningStrength = 0.6
+        end
+        if AutoMine then
+            if MiningThread then
+                task.cancel(MiningThread)
+            end
+            MiningThread = task.spawn(MineOres)
+        end
+    end
+})
+
+local CollectOresToggle = MineTab:AddToggle({Name = "Collect Ores",  Default = false,  Callback = function(bool)
+    ColOres = bool
+    if ColOres then
+        OresThread = task.spawn(CollectOres)
+        OrionLib:MakeNotification({
+            Name = "Collecting Ores",
+            Content = "Auto Ore Collecting is now active. (Might Cause Slight Lag)",
+            Image = "rbxassetid://4483345998",
+            Time = 3
+        })
+    else
+        if OresThread then
+            task.cancel(OresThread)
+            OresThread = nil
+            OrionLib:MakeNotification({
+                Name = "Collecting Ores",
+                Content = "Auto Ore Collecting is now disabled.",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        end
+    end
+end})
+
+OrionLib:Init()
